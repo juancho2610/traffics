6e8194ed2b91bec46e2080ee92a56319bf5502d0
juancho2610 <juansi_angel@hotmail.com>
2023-10-24 18:47:45 -0500

    tutorialcompleto

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask , request, jsonify, Response
 from flask_pymongo import PyMongo
 from bson import json_util
+from bson import ObjectId
 
 app = Flask(__name__)
 app.config['MONGO_URI']='mongodb://localhost:27017/trafficProyect'
@@ -42,8 +43,34 @@ def get_users():
 
 @app.route('/usuarios/<id>', methods=['GET'])
 def get_user(id):
-    print(id)
-    return 'received'
+    usuario = mongo.db.usuarios.find_one({'_id': ObjectId(id)})
+    response = json_util.dumps(usuario)
+    return Response(response, mimetype="application/json")
 
+@app.route('/usuarios/<id>', methods=['DELETE'])
+def delete_user(id):
+    mongo.db.usuarios.delete_one({'_id': ObjectId(id)})
+    response = jsonify({'mensaje': 'usuario' + id + 'ha sido eliminado exitosamente.'})
+    return response
+
+@app.route('/usuarios/<id>', methods = ['PUT'])
+def update_user(id):
+    nombre = request.json['nombre']
+    edad = request.json['edad']
+    correo = request.json['correo']
+    telefono = request.json['telefono']
+    direccion = request.json['direccion']
+    
+    if nombre and edad and correo and telefono and direccion:
+        mongo.db.usuarios.update_one({'_id': ObjectId(id)}, {'$set': {
+            'nombre': nombre,
+            'edad': edad,
+            'correo': correo,
+            'telefono': telefono,
+            'direccion': direccion
+        }})
+        response = jsonify({'mensaje': 'usuario' + id + 'se actualizado correctamente'})
+        return response
+    
 if __name__ == "__main__":
     app.run(debug=True)
